f6866eceaf08ca32ccadc93872bffc0604463eaa
swapnil <sutarswapnil322@gmail.com>
2024-05-29 00:06:26 +0530

    add msg hii

--- 00_Smmary.js
+++ 00_Smmary.js
@@ -1,18 +1,17 @@
 // Primitive DataTypes : 7
 // string, Number, Boolean, null, Undefined, Symbol,BigInt
 
-// Refernce DataType (non-Primitive) : 3 
+// Refernce DataType (non-Primitive) : 3
 // Array, Objects, Functions
-
 
 // there are two types of memory
 
 // 1. stack memory (primitive)
 
-let Myname = "swapnnl"
-let anotherName= Myname
+let Myname = "swapnnl";
+let anotherName = Myname;
 
-anotherName="swappy"
+anotherName = "swappy";
 
 console.log(Myname);
 console.log(anotherName);
@@ -20,12 +19,13 @@ console.log(anotherName);
 // 2. heap memory (non-Primitive)
 
 let info = {
-    email : "swap2gmail.com",
-    id: "01"
-}
+  email: "swap2gmail.com",
+  id: "01",
+};
 
-let newInfo=info;
+let newInfo = info;
 newInfo.email = "Swapnil@.com";
 
 console.log(info.email);
 console.log(newInfo.email);
+console.log("hii");
