c0453e865419980a715b9dbd66d4cce52dca1199
swapnil <sutarswapnil322@gmail.com>
2024-08-04 20:50:04 +0530

    dom

--- 1_Basics/09.0_objects.js
+++ 1_Basics/09.0_objects.js
@@ -1,24 +1,24 @@
-const pass =Symbol("password")
-const jsUser={
-    name: "swapnil",
-    "full name":"swapnil sutar",
-    [pass]:"Swap@123",//symbol
-    age:22,
-    location:"kolhapur",
-    email:"swap@gmail.com",
-    loggredIn:false,
-    day:["mon", "thu","fri"]
-}
+const pass = Symbol("password");
+
+const jsUser = {
+  name: "swapnil",
+  "full name": "swapnil sutar",
+  [pass]: "Swap@123", //symbol
+  age: 22,
+  location: "kolhapur",
+  email: "swap@gmail.com",
+  loggredIn: false,
+  day: ["mon", "thu", "fri"],
+};
 
 console.log(jsUser);
 console.log(jsUser.name);
 console.log(jsUser.location);
-console.log(jsUser["full name"]);
 
 // value change
 
 jsUser.age = 23;
 console.log(jsUser.age);
 
-jsUser.pass = "Swapnil#123"
+jsUser.pass = "Swapnil#123";
 console.log(jsUser.pass);
--- 1_Basics/12_scopes.js
+++ 1_Basics/12_scopes.js
@@ -1,59 +1,56 @@
-
-  if (true) {
-    let a =10;
-    const b = 20;
-    var c = 30;  
-  }
-
-//   console.log(a); 
-//   console.log(b);
-  console.log(c); //var cha Scope {} chy baher pn yeto
-  
-
-//   block scope or global scope 
-
-let a = 300;     //global scope 
 if (true) {
-    let a = 10;   //block scope
-    console.log("inner a is:",a);   
+  let a = 10;
+  const b = 20;
+  var c = 30;
+}
+
+//   console.log(a);
+//   console.log(b);
+console.log(c); //var cha Scope {} chy baher pn yeto
+
+//   block scope or global scope
+
+let a = 300; //global scope
+if (true) {
+  let a = 10; //block scope
+  console.log("inner a is:", a);
 }
 
 // *********************************
-function one(){
-    const username = "swap"
+function one() {
+  const username = "swap";
 
-    function two(){
-      const website = "vscode"
-      console.log(website);
-      console.log(username);
-
-    }
-    two()
-    // console.log(website);
+  function two() {
+    const website = "vscode";
+    console.log(website);
+    console.log(username);
+  }
+  two();
+  // console.log(website);
 }
 
-one() 
+one();
 
 // **************************************
 
 if (true) {
   const name = "swap";
-    if (name == "swap") {
-      const web = "swappy.com";
-      console.log(`your name is ${name} and full website name is ${web}`);
-    }
-} 
+  if (name == "swap") {
+    const web = "swappy.com";
+    console.log(`your name is ${name} and full website name is ${web}`);
+  }
+}
 // ************************************
 
 console.log(oneFun(5));
 
-function oneFun(num){
-  return num+1;
+function oneFun(num) {
+  return num + 1;
 }
 
-const twoFun = function(num){
-  return num+2;
-}
+const twoFun = function (num) {
+  return num + 2;
+};
 console.log(twoFun(6));
 
 // function aa(){
@@ -63,6 +60,6 @@ console.log(twoFun(6));
 
 // if(true){
 //   var ab1=10;
-  
+
 // }
 // console.log(ab1);
